a9cc6c01f1a0052a71e6c4d16bc1397538fd27b0
Matteo Barzaghi <matteo.barzaghi91@gmail.com>
2022-09-08 11:54:25 +0200

    Golang: introduction to Scan() function (user input) and pointers

--- main.go
+++ main.go
@@ -3,17 +3,36 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Print("Hello Go!")
 
 	// variables
 	var conferenceName = "Go Conference"
 
 	// constants
-	const conferenceTickets = 50
-	//var remainingTickets = 50
+	//const conferenceTickets = 50
+	var remainingTickets int = 50
 
 	fmt.Println("Welcome to the", conferenceName, "booking application")
 	fmt.Println("Get yuor tickets here to attend!")
-	fmt.Println(conferenceTickets, "tickets are left.")
+	fmt.Println(remainingTickets, "tickets are left.")
+
+	// input: ask user for their name
+	var userName string
+	var userLastName string
+	var userEmail string
+	var userWantedTickets int
+
+	fmt.Println("What is your name?")
+	fmt.Scan(&userName)
+	fmt.Println("What is your last name?")
+	fmt.Scan(&userLastName)
+	fmt.Println("What is your email?")
+	fmt.Scan(&userEmail)
+	fmt.Println("How many tickets do you want?")
+	fmt.Scan(&userWantedTickets)
+	fmt.Println("Hello ", userName, " ", userLastName, "!\nWe sent an email to your address:", userEmail, ". Please verify it.\nWe're processing yuor request for", userWantedTickets, "tickets.")
+	remainingTickets = remainingTickets - userWantedTickets
+	fmt.Println("Tickets left: ", remainingTickets)
 
 }
