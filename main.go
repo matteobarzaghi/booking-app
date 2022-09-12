f84dd338720bc8307cf183caa2075d546c5ec029
Matteo Barzaghi <matteo.barzaghi91@gmail.com>
2022-09-12 16:16:55 +0200

    golang: loops introduced to check tickets

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
@@ -17,28 +20,54 @@ func main() {
 	fmt.Println("Get yuor tickets here to attend!")
 	fmt.Println(remainingTickets, "tickets are left.")
 
-	// input: ask user for their name
-	var userName string
-	var userLastName string
-	var userEmail string
-	var userWantedTickets int
+	for {
 
-	fmt.Println("What is your name?")
-	fmt.Scan(&userName)
-	fmt.Println("What is your last name?")
-	fmt.Scan(&userLastName)
-	fmt.Println("What is your email?")
-	fmt.Scan(&userEmail)
-	fmt.Println("How many tickets do you want?")
-	fmt.Scan(&userWantedTickets)
-	fmt.Println("Hello ", userName, " ", userLastName, "!\nWe sent an email to your address:", userEmail, ". Please verify it.\nWe're processing yuor request for", userWantedTickets, "tickets.")
-	remainingTickets = remainingTickets - userWantedTickets
-	fmt.Println("Tickets left: ", remainingTickets)
+		// input: ask user for their name
+		var userName string
+		var userLastName string
+		var userEmail string
+		var userWantedTickets int
 
-	// Arrays and Slices
+		fmt.Println("What is your name?")
+		fmt.Scan(&userName)
 
-	var bookings []string
-	bookings = append(bookings, userName+" "+userLastName)
-	fmt.Printf("The whole slice: %v\n", bookings)
+		fmt.Println("What is your last name?")
+		fmt.Scan(&userLastName)
 
+		fmt.Println("What is your email?")
+		fmt.Scan(&userEmail)
+
+		fmt.Println("How many tickets do you want?")
+		fmt.Scan(&userWantedTickets)
+
+		if userWantedTickets <= remainingTickets {
+			fmt.Println("Hello ", userName, " ", userLastName, "!\nWe sent an email to your address:", userEmail, ". Please verify it.\nWe're processing yuor request for", userWantedTickets, "tickets.")
+			remainingTickets = remainingTickets - userWantedTickets
+
+			fmt.Println("Tickets left: ", remainingTickets)
+
+			// Arrays and Slices
+
+			var bookings []string
+			bookings = append(bookings, userName+" "+userLastName)
+
+			// loop to print only names
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+			if remainingTickets <= 0 {
+				// end program
+				fmt.Println("The conference is booked out, come back next year.")
+				break
+			}
+
+		} else {
+			fmt.Printf("We only have %v tickets remaining, you can't book %v tickets\n", remainingTickets, userWantedTickets)
+		}
+	}
 }
